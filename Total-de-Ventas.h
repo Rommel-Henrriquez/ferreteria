81ff82eaddd4d6f799456c1f4bca6a243681bfeb
Rommel-Henrriquez <110572905+Rommel-Henrriquez@users.noreply.github.com>
2022-08-08 20:01:08 -0600

    Create Total-de-Ventas.h
    Función para las ventas totales

---
+++ Total-de-Ventas.h
@@ -0,0 +1,164 @@
+#include "Total-de-Ventas.h"
+int main (){
+    
+    int totalCompra, unidad; 
+    int totalArticulo1, totalArticulo2, totalArticulo3, totalArticulo4, totalArticulo5;
+    int totalArticulo6, totalArticulo7, totalArticulo8, totalArticulo9, totalArticulo10;
+    int totalArticulo11, totalArticulo12, totalArticulo13, totalArticulo14, totalArticulo15;
+    int totalArticulo16, totalArticulo17, totalArticulo18, totalArticulo19, totalArticulo20;
+    int totalArticulo21, totalArticulo22, totalArticulo23, totalArticulo24, totalArticulo25;
+    int totalArticulo26, totalArticulo27
+    
+   // ***************Herramientas******************** 
+    if (articulo == tenaza){
+        cout << "El total de gastos en Tenaza es: " << totalArticulo ;
+        totalArticulo1 = 314.79 * Unidad;
+    }
+
+    if (articulo == taladro){
+        cout << "El total de gastos en Tenaza es: " << totalArticulo ;
+        totalArticulo2 = 866.14 * Unidad;
+    }
+    
+    if (articulo == sierra){
+        cout << "El total de gastos en Sierra es: " << totalArticulo ;
+        totalArticulo3 = 124.84 * Unidad;
+    }
+    if (articulo == martillo){
+        cout << "El total de gastos en Martillo es: " << totalArticulo ;
+        totalArticulo4 = 149.49 * Unidad;
+    }
+
+    
+    //******************Fontaneria*********************
+    if (articulo == tubo){
+        cout << "El total de gastos en Tubo es: " << totalArticulo ;
+        totalArticulo5 = 180.89 * Unidad;
+    }
+
+    if (articulo == codos){
+        cout << "El total de gastos en Codos es: " << totalArticulo ;
+        totalArticulo6 = 32.33 * Unidad;
+    }
+    
+    if (articulo == desague){
+        cout << "El total de gastos en Desague es: " << totalArticulo ;
+        totalArticulo7 = 96.15 * Unidad;
+    }
+    if (articulo == grifo){
+        cout << "El total de gastos en Grifo es: " << totalArticulo ;
+        totalArticulo8 = 425.56 * Unidad;
+    }
+    
+    //*****************Construccion***********************
+    if (articulo == varilla){
+        cout << "El total de gastos en Varilla es: " << totalArticulo ;
+        totalArticulo9 = 217.82 * Unidad;
+    }
+
+    if (articulo == grava){
+        cout << "El total de gastos en Grava es: " << totalArticulo ;
+        totalArticulo9 = 380.12 * Unidad;
+    }
+    
+    if (articulo == arena){
+        cout << "El total de gastos en Arena es: " << totalArticulo ;
+        totalArticulo10 = 10.23 * Unidad;
+    }
+    if (articulo == cemento){
+        cout << "El total de gastos en Grifo es: " << totalArticulo ;
+        totalArticulo11 = 215.63 * Unidad;
+    }
+    
+    //******************Electricidad**************************
+    if (articulo == cableElectrico){
+        cout << "El total de gastos en Cable Electrico es: " << totalArticulo ;
+        totalArticulo12 = 83.34 * Unidad;
+    }
+
+    if (articulo == cintaAislante){
+        cout << "El total de gastos en Cinta Aislante es: " << totalArticulo ;
+        totalArticulo13 = 560.49 * Unidad;
+    }
+    
+    if (articulo == lampara){
+        cout << "El total de gastos en Lampara es: " << totalArticulo ;
+        totalArticulo14 = 569.99 * Unidad;
+    }
+    if (articulo == manguera){
+        cout << "El total de gastos en Manguera es: " << totalArticulo ;
+        totalArticulo15 = 6.15 * Unidad;
+    }    
+    
+    //*********************Hogar**************************
+        if (articulo == cortina){
+        cout << "El total de gastos en Cortina es: " << totalArticulo ;
+        totalArticulo16 = 1069.56 * Unidad;
+    }
+
+    if (articulo == puerta){
+        cout << "El total de gastos en Grava es: " << totalArticulo ;
+        totalArticulo17 = 1747.22 * Unidad;
+    }
+    
+    if (articulo == ventilador){
+        cout << "El total de gastos en Ventilador es: " << totalArticulo ;
+        totalArticulo18 = 3717.85 * Unidad;
+    }
+    if (articulo == mueble){
+        cout << "El total de gastos en Mueble es: " << totalArticulo ;
+        totalArticulo19 = 17590.43 * Unidad;
+    }
+    
+    //******************Jardineria************************
+    if (articulo == pico){
+        cout << "El total de gastos en Pico es: " << totalArticulo ;
+        totalArticulo20 = 375.23 * Unidad;
+    }
+
+    if (articulo == pala){
+        cout << "El total de gastos en Pala es: " << totalArticulo ;
+        totalArticulo21 = 300.12 * Unidad;
+    }
+    
+    if (articulo == azadon){
+        cout << "El total de gastos en Azadon es: " << totalArticulo ;
+        totalArticulo22 = 155.17 * Unidad;
+    }
+    if (articulo == machete){
+        cout << "El total de gastos en Grifo es: " << totalArticulo ;
+        totalArticulo23 = 88.19 * Unidad;
+    }
+    
+    //*****************Otros*******************
+        if (articulo == brocha){
+        cout << "El total de gastos en Brocha es: " << totalArticulo ;
+        totalArticulo24 = 41.50 * Unidad;
+    }
+
+    if (articulo == rodilloyBandeja){
+        cout << "El total de gastos en Rodillo y Bandeja es: " << totalArticulo ;
+        totalArticulo25 = 114.30 * Unidad;
+    }
+    
+    if (articulo == escalera){
+        cout << "El total de gastos en Escalera es: " << totalArticulo ;
+        totalArticulo26 = 3540.50 * Unidad;
+    }
+    if (articulo == galonPintura){
+        cout << "El total de gastos en Galon de Pintura es: " << totalArticulo ;
+        totalArticulo27 = 950.60 * Unidad;
+    }
+
+    //Suma total
+    totalCompra = totalArticulo1 + totalArticulo2 + totalArticulo3, totalArticulo4 + totalArticulo5 + 
+    totalArticulo6 + totalArticulo7 + totalArticulo8 + totalArticulo9 + totalArticulo10 + 
+    totalArticulo11 + totalArticulo12 + totalArticulo13, totalArticulo14 + totalArticulo15 + 
+    totalArticulo16 + totalArticulo17 + totalArticulo18 + totalArticulo19 + totalArticulo20 +
+    totalArticulo21 + totalArticulo22 + totalArticulo23, totalArticulo24 + totalArticulo25 + 
+    totalArticulo26 + totalArticulo27;
+  
+    cout << "TOTAL DE LA COMPRA: " << totalCompra;
+
+    return 0;
+}
